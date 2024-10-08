2f43cd0bc7737fc979c34b36771c32a8efd15458
hasibhu <hasibul1987@gmail.com>
2024-10-08 09:30:36 +0200

    Navbar options have been repositioned

--- src/components/NavLinks.jsx
+++ src/components/NavLinks.jsx
@@ -3,8 +3,8 @@ import { NavLink } from 'react-router-dom';
 
 const links = [
   { id: 1, url: '/', text: 'home' },
-  { id: 2, url: 'about', text: 'about' },
-  { id: 3, url: 'products', text: 'products' },
+  { id: 2, url: 'products', text: 'products' },
+  { id: 3, url: 'about', text: 'about Us' },
   { id: 4, url: 'cart', text: 'cart' },
   { id: 5, url: 'checkout', text: 'checkout' },
   { id: 6, url: 'orders', text: 'orders' },
