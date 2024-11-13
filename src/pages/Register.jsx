4828c55af9f3ba993f308055dfe9ae6c78c6da32
hasibhu <hasibul1987@gmail.com>
2024-11-14 00:21:13 +0100

    registering is done

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,9 +1,23 @@
 import React from 'react';
-import { Form, Link } from 'react-router-dom';
+import { Form, Link, redirect } from 'react-router-dom';
 import { FormInput, SubmitButton } from '../components';
+import { customFetch } from '../utils';
 
 
-export const action = async () => {
+export const action = async ({ request }) => {
+    const formData = await request.formData();
+    const data = Object.fromEntries(formData)
+
+    try {
+        const response = await customFetch.post('/auth/local/register', data);
+        alert('Account created successfully!!')
+        return redirect('/login')
+    } catch (error) {
+        console.log(error);
+    }
+
+
+
     return null;
 }
 const Register = () => {
